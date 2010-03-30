5d11cb9094aea64582feeaaa70192b97dd5d3e64
Henrik Johansson <henrik@arcturus.(none)>
2010-03-30 19:19:20 +0200

    Added debian control files

---
+++ debian/liboa.default.ex
@@ -0,0 +1,10 @@
+# Defaults for liboa initscript
+# sourced by /etc/init.d/liboa
+# installed at /etc/default/liboa by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
